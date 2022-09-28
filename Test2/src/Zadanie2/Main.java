d3fa508b43cf8bbf0685eb95f8e9befc94eb3b0c
s25526-pj <92204707+s25526-pj@users.noreply.github.com>
2022-09-28 22:08:34 +0200

    Add files via upload

--- Test2/src/Zadanie2/Main.java
+++ Test2/src/Zadanie2/Main.java
@@ -6,18 +6,12 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-
-        Calculator calculator1 = (a, b) -> a + b;
-        Calculator calculator2 = (a, b) -> a - b;
-        Calculator calculator3 = (a, b) -> a * b;
-        Calculator calculator4 = (a, b) -> a / b;
-
-
+        
         Map<String, Calculator> operationMap = new HashMap<>();
-        operationMap.put("+", calculator1);
-        operationMap.put("-", calculator2);
-        operationMap.put("*", calculator3);
-        operationMap.put("/", calculator4);
+        operationMap.put("+", (a, b) -> a + b);
+        operationMap.put("-", (a, b) -> a - b);
+        operationMap.put("*", (a, b) -> a * b);
+        operationMap.put("/", (a, b) -> a / b);
         System.out.println(operationMap.get("+").calculate(10, 20));
 
     }
